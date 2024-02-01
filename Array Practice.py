1640238a29c79afa1858a7119ff287ff058082fd
SonnyTaylor <santinotaylor08@gmail.com>
2024-02-02 09:44:46 +1100

    Added for loop

--- Array Practice.py
+++ Array Practice.py
@@ -1,8 +1,9 @@
+# Initialise the list with the following items: "Apples", "Milk", "Pasta", "Bread", "Eggs"
 grocery_list = ["Apples", "Milk", "Pasta", "Bread", "Eggs"]
 
 # First item in the array wich would be "Apples"
 first_item = grocery_list[0]
-print(first_item)
+print("this is the first item:", first_item)
 
 # Get input for what to add to the list and append it
 grocery_list_input = input("What do you want to add to the list? ")
@@ -17,4 +18,8 @@ print(grocery_list)
 # Sort the list in alphabetical order and print it
 grocery_list.sort()
 
-print("this is the sorted grocerylist:", grocery_list)
+print("this is the sorted grocery list:", grocery_list)
+
+# Itterate through array with a for loop
+for each in grocery_list:
+    print(each)
