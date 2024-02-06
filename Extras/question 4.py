5ee6f38d1b0a0a9f325a8ba2f65d47befac585a2
SonnyTaylor <santinotaylor08@gmail.com>
2024-02-06 14:56:09 +1100

    more work

---
+++ Extras/question 4.py
@@ -0,0 +1,22 @@
+# The All-Star Sports Club needs to find out who is eligible to enter an upcoming Masters event.
+# All participants in the Masters event must be 40 years old or olden The club needs to print out a list of
+# eligible members.
+# When the club's system is running, all data is stored in a series of arrays. The relevant fields are
+# date_of_birth[], surname[], firstname[] and gender[], all with the same index value for all fields ofa member.
+# The variable numbermembers holds the number of members of the club and the arrays start at the value 1.
+# It has been proposed to add a procedure that will start at the first record and look through each record,
+# printing out the first name, surname, gender and date of birth of each person who is at least 40 years old.
+# To compute age, all members must be born before 1977.
+
+
+date_of_birth = [1970, 1975, 1980, 1985, 1990]
+surname = ["Smith", "Jones", "Taylor", "Brown", "Williams"]
+firstname = ["John", "David", "Peter", "Paul", "Simon"]
+gender = ["M", "M", "M", "M", "M"]
+numbermembers = 5
+
+for i in range(1, numbermembers + 1):
+    if 2024 - date_of_birth[i - 1] >= 40:
+        print(
+            f"First Name: {firstname[i - 1]}, Surname: {surname[i - 1]}, Gender: {gender[i - 1]}, Date of Birth: {date_of_birth[i - 1]}"
+        )
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Applied Computing Work Repo
 
 ## this is my school work, its just easier to make it a git repo lol
+
+note to me: dont change folder names, it will stuff up urls
