b88eeec227cb532d9b101c91a13ba20762942e07
SonnyTaylor <santinotaylor08@gmail.com>
2024-02-07 10:35:11 +1100

    added docstring to celcius to farenheit

--- Extras/celcius_to_farenheit.py
+++ Extras/celcius_to_farenheit.py
@@ -1,4 +1,12 @@
 def celcius_to_farenheit(celcius):
+    """Converts celcius to farenheit
+
+    Args:
+        celcius (float): The temperature in celcius
+
+    Returns:
+        float: Tempature in farenheit
+    """
     farenheit = (celcius * 9 / 5) + 32
     return farenheit
 
