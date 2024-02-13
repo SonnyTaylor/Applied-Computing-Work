7c03239f5bf3af3eb90bad6cf14652471865ad92
SonnyTaylor <santinotaylor08@gmail.com>
2024-02-13 14:45:09 +1100

    Add car functionality and create a new car instance

--- Extras/main_oop_module_1.py
+++ Extras/main_oop_module_1.py
@@ -4,6 +4,13 @@ my_car = Car("Toyota", "Corolla", 2015, "Red")
 
 print("My car: ")
 print(my_car.make)
+print(my_car.model)
+print(my_car.year)
+print(my_car.color)
+my_car.drive()
+my_car.stop()
+
+my_other_car = Car("Ford", "Fiesta", 2018, "Blue")
 print("\nMy other car: ")
 print(my_other_car.make)
 print(my_other_car.model)
