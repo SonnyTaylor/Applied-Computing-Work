81668dde7dbbecb22cd1250bcaef6a4038b547b1
SonnyTaylor <santinotaylor08@gmail.com>
2024-02-13 14:31:33 +1100

    more oop

---
+++ Extras/main_oop_module_1.py
@@ -0,0 +1,13 @@
+from oop_module_1 import *
+
+my_car = Car("Toyota", "Corolla", 2015, "Red")
+
+print("My car: ")
+print(my_car.make)
+print("\nMy other car: ")
+print(my_other_car.make)
+print(my_other_car.model)
+print(my_other_car.year)
+print(my_other_car.color)
+my_other_car.drive()    
+my_other_car.stop()
