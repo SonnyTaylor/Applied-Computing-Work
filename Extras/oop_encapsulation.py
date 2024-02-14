c718068ed88b5ae368b87ab3aa6d8131f9b0282a
SonnyTaylor <santinotaylor08@gmail.com>
2024-02-14 11:11:54 +1100

    oop encapsulation

---
+++ Extras/oop_encapsulation.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.__account_number = account_number  # Private attribute
+        self.__balance = balance  # Private attribute
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("Insufficient balance!")
+
+    def get_balance(self):
+        return self.__balance
+
+
+# Example usage
+account = BankAccount("123456", 100)
+print(account.get_balance())  # 100
+account.deposit(50)
+print(account.get_balance())  # 150
+account.withdraw(30)
+print(account.get_balance())  # 120
+account.withdraw(200)  # Insufficient balance!
+print(account.get_balance())  # 120
