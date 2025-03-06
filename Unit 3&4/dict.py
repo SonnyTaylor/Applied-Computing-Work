23f8e3cc624c2dde3d8b3fe25c1e50a4bdad003c
SonnyTaylor <santinotaylor08@gmail.com>
2025-03-07 09:11:43 +1100

    Remove selection sort implementation and add pseudocode; create a dictionary for person details

---
+++ Unit 3&4/dict.py
@@ -0,0 +1,9 @@
+"""
+Create a dictionary that represents the details of a person with their name, age and city.
+Then write some code to do the following:
+        • Print the dictionary
+        • Update the age
+        • Add their country to the dictionary
+"""
+
+person_details - {"name": "Joe Nutz"}
