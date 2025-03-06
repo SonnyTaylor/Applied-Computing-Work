3c82da3c362bd31b5bd3ff4cb72ce57faa67824c
SonnyTaylor <santinotaylor08@gmail.com>
2025-03-07 09:22:54 +1100

    activity 1 and 2

--- Unit 3&4/dict.py
+++ Unit 3&4/dict.py
@@ -1,9 +1,26 @@
-"""
-Create a dictionary that represents the details of a person with their name, age and city.
-Then write some code to do the following:
-        • Print the dictionary
-        • Update the age
-        • Add their country to the dictionary
-"""
+def activity_1():
+    """
+	Create a dictionary that represents the details of a person with their name, age and city.
+	Then write some code to do the following:
+			• Print the dictionary
+			• Update the age
+			• Add their country to the dictionary
+	"""
+    person_details = {"name": "Joe Nutz", "age": 23, "city": "Melbourne"}
 
-person_details - {"name": "Joe Nutz"}
+    print(person_details)
+    person_details["age"] = 24
+    person_details["country"] = "AU"
+
+def activity_2():
+    animals = {
+		"lion": "mammal",
+		"crocodile": "reptile",
+		"sparrow": "bird",
+		"trout": "fish",
+		"komodo dragon": "reptile",
+	}
+	for animal, type in animals.items():
+		if type == "reptile":
+			print(animal)
+   
