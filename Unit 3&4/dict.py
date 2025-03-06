27afba4dd3dd2b6edc56f11b92973aec58aa06be
SonnyTaylor <santinotaylor08@gmail.com>
2025-03-07 09:32:02 +1100

    Refactor activity_1 and activity_2 functions to improve code clarity; update person details and collect reptiles in a list

--- Unit 3&4/dict.py
+++ Unit 3&4/dict.py
@@ -1,26 +1,30 @@
 def activity_1():
     """
-	Create a dictionary that represents the details of a person with their name, age and city.
-	Then write some code to do the following:
-			• Print the dictionary
-			• Update the age
-			• Add their country to the dictionary
-	"""
+    Create a dictionary that represents the details of a person with their name, age and city.
+    Then write some code to do the following:
+                    • Print the dictionary
+                    • Update the age
+                    • Add their country to the dictionary
+    """
     person_details = {"name": "Joe Nutz", "age": 23, "city": "Melbourne"}
 
     print(person_details)
     person_details["age"] = 24
     person_details["country"] = "AU"
 
+
 def activity_2():
     animals = {
-		"lion": "mammal",
-		"crocodile": "reptile",
-		"sparrow": "bird",
-		"trout": "fish",
-		"komodo dragon": "reptile",
-	}
-	for animal, type in animals.items():
-		if type == "reptile":
-			print(animal)
-   
+        "lion": "mammal",
+        "crocodile": "reptile",
+        "sparrow": "bird",
+        "trout": "fish",
+        "komodo dragon": "reptile",
+    }
+
+    reptiles = []
+    for animal, type in animals.items():
+        if type == "reptile":
+            reptiles.append(animal)
+
+    print(reptiles)
